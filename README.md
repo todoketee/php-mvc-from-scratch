bb445ee8dfd244764c3273fd59ef9201289dda6c
todokete <fabio-marras@hotmail.com>
2021-06-13 16:51:56 -0300

    MVC Framework

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # php-mvc-from-scratch
+A MVC php website made with the help of the following [tutorial](https://youtu.be/mK7CiJeYOwI).
--- app/init.php
+++ app/init.php
@@ -1,5 +1,7 @@
 <?php
 
-require "core/app.php";
+require "core/config.php";
 require "core/functions.php";
+require "core/database.php";
 require "core/controller.php";
+require "core/app.php";
