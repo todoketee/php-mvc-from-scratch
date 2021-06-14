20d921189528f5c817f5ce5fa14c8500c7180bdd
todokete <fabio-marras@hotmail.com>
2021-06-14 16:41:19 -0300

    Resized Images

--- app/views/website/index.php
+++ app/views/website/index.php
@@ -6,7 +6,7 @@
         <article>
           <header class="section background-white">
             <div class="line text-center">        
-              <h1 class="text-dark text-s-size-30 text-m-size-40 text-l-size-headline text-thin text-line-height-1">Be More with Less</h1>
+              <h1 class="text-dark text-s-size-30 text-m-size-40 text-l-size-headline text-thin text-line-height-1">Framework from Scratch</h1>
               <p class="margin-bottom-0 text-size-16 text-dark">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.<br>
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod.</p>
             </div>  
